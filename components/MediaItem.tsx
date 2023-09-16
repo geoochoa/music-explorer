8f4fde76bf3be2df7fb7e58e190903632ecf7859
Geovanni Ochoa <geovnchy@gmail.com>
2023-09-15 18:56:54 -0700

    feat: users can search for songs in supabase, debounced to limit api calls

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -24,13 +24,13 @@ const MediaItem: React.FC<MediaItemProps> = ({
   return (
     <div onClick={handleClick}
         className="
-            flex
-            items-center
-            gap-x-3
-            cursor-pointer
-            hover:bg-neutral-800/50
-            w-full
-            p-2
+            flex 
+            items-center 
+            gap-x-3 
+            cursor-pointer 
+            hover:bg-neutral-800/50 
+            w-full 
+            p-2 
             rounded-md
         "
     >
