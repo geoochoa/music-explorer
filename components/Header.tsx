f7ea780dde4eb092cb6d8f3fe22e521419c4dff8
Geovanni Ochoa <geovnchy@gmail.com>
2023-09-16 17:30:51 -0700

    fix: reset player when logging out

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,6 +11,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
 import { toast } from "react-hot-toast";
+import usePlayer from "@/hooks/usePlayer";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -23,13 +24,14 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
     const authModal = useAuthModal();
     const router = useRouter()
+    const player = usePlayer();
 
     const supabaseClient = useSupabaseClient();
     const {user} = useUser();
 
     const handleLogout = async () => {
         const {error} = await supabaseClient.auth.signOut();
-        //todo: reset any playing songs
+        player.reset();
         router.refresh();
 
         if(error){
